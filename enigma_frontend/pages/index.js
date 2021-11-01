fd8375506f3fdf5b0dd1a64a336a38eb6504e8b9
Brandon Unzueta <unzueta.brandon@gmail.com>
2021-10-31 20:52:02 -0700

    starting to design home page

--- enigma_frontend/pages/index.js
+++ enigma_frontend/pages/index.js
@@ -1,9 +1,13 @@
 import Layout from "../components/Layout";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <Layout>
-      <h1>Enigma</h1>
+      <div className={styles.landing_screen}>
+        <h1>Welcome To My</h1>
+        <h1>World</h1>
+      </div>
     </Layout>
   );
 }

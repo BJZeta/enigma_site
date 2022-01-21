884c47d683e4f71e441ebc730c1f4e8c86fe34a7
Brandon Unzueta <unzueta.brandon@gmail.com>
2022-01-20 20:15:45 -0800

    creating sanity.io studio for backend

---
+++ studio/schemas/art.js
@@ -0,0 +1,76 @@
+export default {
+  name: "art",
+  title: "Art Piece",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+    },
+    {
+      name: "mainImage",
+      title: "Main image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "categories",
+      title: "Categories",
+      type: "array",
+      of: [{ type: "text" }],
+    },
+    {
+      name: "type",
+      title: "Type",
+      type: "array",
+      of: [{ type: "reference", to: { type: "types" } }],
+    },
+    {
+      name: "publishedAt",
+      title: "Published at",
+      type: "datetime",
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "text",
+    },
+    {
+      name: "for_sale",
+      title: "For Sale",
+      type: "boolean",
+    },
+    {
+      name: "sale_link",
+      title: "Sale Link",
+      type: "string",
+    },
+  ],
+
+  /////UNEEDED PREVIEW FOR THE TIME BEING////
+  // preview: {
+  //   select: {
+  //     title: "title",
+  //     media: "mainImage",
+  //   },
+  //   prepare(selection) {
+  //     const { author } = selection;
+  //     return Object.assign({}, selection, {
+  //       subtitle: author && `by ${author}`,
+  //     });
+  //   },
+  // },
+  /////UNEEDED PREVIEW FOR THE TIME BEING////
+};
---
+++ studio/schemas/types.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'types',
+  title: 'Types',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    },
+  ],
+}

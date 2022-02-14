4b91538622ae66793964d8449ed487b68d058c59
Brandon Unzueta <unzueta.brandon@gmail.com>
2022-02-13 22:00:44 -0800

    adding next/image

--- frontend/components/ArtPiece.js
+++ frontend/components/ArtPiece.js
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import Image from "next/image";
+import { useNextSanityImage } from "next-sanity-image";
 import client from "../client";
 import imageUrlBuilder from "@sanity/image-url";
 import styles from "../styles/ArtPiece.module.css";
@@ -8,10 +10,25 @@ function urlFor(source) {
 }
 
 export default function ArtPiece({ _id, slug, mainImage, title }) {
+  const imageProps = useNextSanityImage(client, mainImage);
+
+  // const { blurDataURL, src, width, height } = imageProps;
+
+  console.log(imageProps);
+
   return (
     <Link href="/gallery/[slug]" as={`/gallery/${slug.current}`} key={_id}>
       <a className={styles.piece}>
-        <img src={urlFor(mainImage)} alt={title} />
+        <div className={styles.image}>
+          <Image
+            src={imageProps.src}
+            loader={imageProps.loader}
+            width={imageProps.width}
+            height={imageProps.height}            
+            layout="responsive"
+            objectFit="contain"
+          />
+        </div>
         <h3>{title}</h3>
       </a>
     </Link>
